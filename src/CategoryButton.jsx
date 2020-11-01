ccb6f52108875e8d520d2e6023353d664c69f96c
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-11-01 00:12:29 +0000

    table added

--- src/CategoryButton.jsx
+++ src/CategoryButton.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import TableElement from './TableElement';
 
 
 const CategoryButtons = () => {
-    
+
   return (
-    <div className='buttons-wrapper'>
-        <button type="button" className="btn btn-info btn-lg">Pharmacies</button>
-        <button type="button" className="btn btn-info btn-lg">schools&colleges</button>
-        <button type="button" className="btn btn-info btn-lg">hospitals</button>
-        <button type="button" className="btn btn-info btn-lg">doctors</button>
+    <div className='category-button' >
+        <div className='buttons-wrapper' >
+            <button type="button" className="btn btn-info btn-lg">Pharmacies</button>
+            <button type="button" className="btn btn-info btn-lg">schools&colleges</button>
+            <button type="button" className="btn btn-info btn-lg">hospitals</button>
+            <button type="button" className="btn btn-info btn-lg">doctors</button>
+        </div>
+        <TableElement/>
     </div>
   );
 };
---
+++ src/TableElement.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const TableElement = () => {
+
+  return (
+    <div>
+<table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Address</th>
+      <th scope="col">Website</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>0747366543</td>
+      <td>10 York Close, Downend, London</td>
+       <td>HeathCare Ltd.</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>0747366543</td>
+      <td>1 Brandon Close, Dayton, London</td>
+       <td>HeathCare Ltd.</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Jacob</td>
+      <td>0747366543</td>
+      <td>1 Brandon Close, Dayton, London</td>
+       <td>HeathCare Ltd.</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+  );
+};
+
+export default TableElement;
